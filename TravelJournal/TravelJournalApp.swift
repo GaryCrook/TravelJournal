d8e4f48090bc4c8a3165434ab3b1537ee168e283
Gary Crook <gcrook@gmail.com>
2026-06-13 08:57:53 +0100

    Initial Commit

---
+++ TravelJournal/TravelJournalApp.swift
@@ -0,0 +1,32 @@
+//
+//  TravelJournalApp.swift
+//  TravelJournal
+//
+//  Created by Gary Crook on 13/06/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TravelJournalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
